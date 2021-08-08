e0acb9bb4035289aadd5bb3665ded9cbe23be9e9
Oshten <mereguan55@yandex.ru>
2021-08-08 15:14:40 +0300

    Для одного сайта работает

--- parser.py
+++ parser.py
@@ -1,33 +1,74 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.wildberries.ru/brands/polezzno'
+URL_WILDBERRIES = 'https://www.wildberries.ru/brands/polezzno'
 HEADERS = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/91.0.4472.135 YaBrowser/21.6.2.855 Yowser/2.5 Safari/537.36', 'accept' : '*/*'}
 
-SEARCHED_ELEMENT = 'Чай матча зеленая/рассыпной'
+SEARCHED_ELEMENT = 'Чай матча сломатча зеленая/рассыпной'
+
+HOST_WILDBERRIES = 'https://www.wildberries.ru'
+
+FILE = 'Информация о ценах.txt'
 
 def get_html(url, params = None):
     result = requests.get(url=url, headers = HEADERS, params=params)
     return result
 
+def get_pagination_next(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    page_next = HOST_WILDBERRIES + soup.find('a', class_='pagination-next').get('href')
+    return page_next
+
 def get_content(html):
+    print('Идет парсинг ...')
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('a', class_='ref_goods_n_p j-open-full-product-card')
-    # print(items)
     for item in items:
-        if item.find('span', class_='goods-name c-text-sm').get_text(strip=True) == SEARCHED_ELEMENT:
-            link = item.get('href')
-            prise = item.find('span', class_='price')
-            print(link, prise)
-            break
+        # print(item.find('span', class_='goods-name').get_text(strip=True))
+        if item.find('span', class_='goods-name').get_text(strip=True) == SEARCHED_ELEMENT:
+            # print(item.find('span', class_='goods-name').get_text(strip=True))
+            link = HOST_WILDBERRIES + item.get('href')
+            prise = item.find('span', class_='price').get_text(strip=True)
+            print(f'Товар на сайте {HOST_WILDBERRIES} найден.')
+            # print(link, prise)
+            return prise, link
+    else:
+        return False
+
+
+
+def record_info(file_info, prise, link):
+    with open(file_info, 'a', encoding='utf8') as file:
+        shope_name = HOST_WILDBERRIES.split('.')[1]
+        file.write(shope_name + '\n' + SEARCHED_ELEMENT + '\n' + prise + '\n' + link + '\n' + '\n\n\n')
 
 
 def parse():
-    html = get_html(url=URL)
+    html = get_html(url=URL_WILDBERRIES)
     if html.status_code == 200:
-        get_content(html=html.text)
+        while True:
+            try:
+                prise, link = get_content(html=html.text)
+                break
+            except:
+                try:
+                    page_next = get_pagination_next(html=html.text)
+                    html = get_html(url=page_next)
+                except AttributeError:
+                    print(f'Товар на сайте {HOST_WILDBERRIES} не найден.')
+                    prise, link = 'Товар отсутствует', 'Товар отсутствует'
+                    break
+        record_info(file_info=FILE, prise=prise, link=link)
     else:
         print('Страница не доступна')
 
+
 parse()
+
+# url = 'https://www.wildberries.ru/brands/polezzno'
+# html = get_html(url=url, params=None)
+# get_content(html=html.text)
+# # print(prise, link)
+# # print(get_pagination_next(html=html.text))
